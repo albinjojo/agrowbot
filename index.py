aedcc409f00c27d6aeeddfce022825d19c7d3c7a
unknown <jojoalbin21@gmail.com>
2026-01-02 20:35:21 +0530

    rag model which reads pdf file

---
+++ index.py
@@ -0,0 +1,40 @@
+from pypdf import PdfReader
+import pickle
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+
+all_chunks = []
+
+pdf_files = [
+    "a0443e04.pdf",
+    "a1193e00.pdf",
+    "cb3963en.pdf",
+    "cc1163en.pdf"
+]
+
+for pdf in pdf_files:
+    reader = PdfReader(pdf)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+
+    words = text.split()
+    for i in range(0, len(words), 500):
+        all_chunks.append(" ".join(words[i:i+500]))
+
+print("Number of chunks:", len(all_chunks))
+# print(all_chunks[0])
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+embeddings = model.encode(all_chunks)
+
+index = faiss.IndexFlatL2(embeddings.shape[1])
+index.add(np.array(embeddings))
+# print("Vectors stored:", index.ntotal)
+
+faiss.write_index(index, "agri.index")
+
+with open("chunks.pkl", "wb") as f:
+    pickle.dump(all_chunks, f)
---
+++ query.py
@@ -0,0 +1,51 @@
+import pickle
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from google import genai
+import os
+
+index = faiss.read_index("agri.index")
+with open("chunks.pkl", "rb") as f:
+    all_chunks = pickle.load(f)
+
+embedder = SentenceTransformer("all-MiniLM-L6-v2")
+
+def retrieve(question, k=3):
+    q_emb = embedder.encode([question])
+    _, ids = index.search(np.array(q_emb), k)
+    return [all_chunks[i] for i in ids[0]]
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+question = "How to control pests in rice?"
+
+context = "\n\n".join(retrieve(question))
+
+instruction = "You are a helpful assistant that answers questions strictly based on the provided context. Extract the most relevant pest control methods for rice from the provided data. Summarize in 2-3 short bullet points only. Be concise. No intro or explanation needed and use phrases if needed. If the context does not contain sufficient information, respond with general, agricultural guidance from your own knowledge, give concrete response instead of asking to user to refer other sources. If the question is not related to agriculture or farming, respond with 'I am sorry, I don't have the information to answer that question.' Don't include any aestrisk marks or other markdown formatting in your response."
+
+prompt = f"""
+{instruction}
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=prompt,
+    config=genai.types.GenerateContentConfig(
+        # max_output_tokens=256,
+        # temperature=0.3,
+        # top_p=0.95,
+        # top_k=40,
+    )
+)
+response_text = response.text.replace("*", "").strip()
+
+print(response_text)
